a0dc93a75753db55e4a2d312504fa97e82f2c852
jaerrib <jaerrib@tutanota.com>
2023-09-17 05:58:24 -0400

    Separate blog from main/index

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.PostList.as_view(), name="home"),
+    path("", views.index, name="home"),
+    path("blog_base/", views.PostList.as_view(), name="blog_index"),
     path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.views import generic
 
 from .models import Post
@@ -5,9 +6,13 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "index.html"
+    template_name = "blog_index.html"
 
 
 class PostDetail(generic.DetailView):
     model = Post
     template_name = "post_detail.html"
+
+
+def index(request):
+    return render(request, "index.html")
